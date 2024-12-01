2cc81467f42fa2bf5e4ea41523a4f1a3a0ce0af8
andrewhamaty <113324413+andrewhamaty@users.noreply.github.com>
2024-12-01 15:12:01 -0500

    .

---
+++ histogram_of_tweets/solution.sql
@@ -0,0 +1,18 @@
+with total_tweets as (
+    select
+        T.user_id,
+        count(T.tweet_id) as tweet_count_per_user,
+    from tweets as T
+    --where year(tweet_date) = '2022'
+    where tweet_date >= '2022-01-01'
+    and tweet_date <= '2022-12-31'
+    group by T.user_id
+)
+select
+    TT.tweet_count_per_user as tweet_bucket,
+    count(TT.user_id) as users_num
+from total_tweets as TT
+group by TT.tweet_count_per_user
+;
+
+-- year() works with other dbs but not on datalemur
