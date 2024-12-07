777a1fe5979311b43e20bc0f3b34ee6733f29ed8
andrewhamaty <113324413+andrewhamaty@users.noreply.github.com>
2024-12-07 14:04:30 -0500

    .

--- average_review_ratings/solution.sql
+++ average_review_ratings/solution.sql
@@ -1,18 +1,3 @@
-CREATE TABLE reviews (
-    review_id INT PRIMARY KEY,
-    user_id INT NOT NULL,
-    submit_date DATETIME NOT NULL,
-    product_id INT NOT NULL,
-    stars INT NOT NULL CHECK (stars BETWEEN 1 AND 5)
-);
-
-INSERT INTO reviews (review_id, user_id, submit_date, product_id, stars) VALUES
-(6171, 123, '2022-06-08 00:00:00', 50001, 4),
-(7802, 265, '2022-06-10 00:00:00', 69852, 4),
-(5293, 362, '2022-06-18 00:00:00', 50001, 3),
-(6352, 192, '2022-07-26 00:00:00', 69852, 3),
-(4517, 981, '2022-07-05 00:00:00', 69852, 2);
-
 select
     --month(R.submit_date) as mth,
     extract(month from R.submit_date) as mth,
